d00d6081f97f54ae7be6ed9abbe6ba1a0e8996e3
Anderson Lagare Antigua <antiguaandersonph01@gmail.com>
2026-04-05 21:19:14 +0800

    Add query for top employees by order count
    Added a query to find the employee who processed the most orders.

--- 02_top_employees_by_orders.sql
+++ 02_top_employees_by_orders.sql
@@ -1,1 +1,10 @@
+-- Query 2: Which employee processed the most orders?
+-- Dataset: Northwind | Tool: SQLiteOnline.com
 
+SELECT 
+    Employees.FirstName || ' ' || Employees.LastName AS EmployeeName,
+    COUNT(Orders.OrderID) AS TotalOrders
+FROM Orders
+JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+GROUP BY Employees.EmployeeID
+ORDER BY TotalOrders DESC;
