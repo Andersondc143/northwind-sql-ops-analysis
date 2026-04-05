686b7249f12f5e15e191331322b1350c3d4d83d0
Anderson Lagare Antigua <antiguaandersonph01@gmail.com>
2026-04-05 21:19:28 +0800

    Update 03_shipper_fulfillment_speed.sql

--- 03_shipper_fulfillment_speed.sql
+++ 03_shipper_fulfillment_speed.sql
@@ -1,1 +1,12 @@
+-- Query 3: Which shipper has the fastest average fulfillment time?
+-- Dataset: Northwind | Tool: SQLiteOnline.com
 
+SELECT 
+    Shippers.CompanyName AS Shipper,
+    ROUND(AVG(JULIANDAY(Orders.ShippedDate) - JULIANDAY(Orders.OrderDate)), 1) AS AvgDaysToShip,
+    COUNT(Orders.OrderID) AS TotalOrders
+FROM Orders
+JOIN Shippers ON Orders.ShipVia = Shippers.ShipperID
+WHERE Orders.ShippedDate IS NOT NULL
+GROUP BY Shippers.ShipperID
+ORDER BY AvgDaysToShip ASC;
