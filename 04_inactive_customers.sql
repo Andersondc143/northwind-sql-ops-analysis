461cf45ca1ce164f9247dff9b5bdd81ca045c466
Anderson Lagare Antigua <antiguaandersonph01@gmail.com>
2026-04-05 21:19:41 +0800

    Update 04_inactive_customers.sql

--- 04_inactive_customers.sql
+++ 04_inactive_customers.sql
@@ -1,1 +1,12 @@
+-- Query 4: Which customers have been inactive for 6+ months?
+-- Dataset: Northwind | Tool: SQLiteOnline.com
 
+SELECT 
+    Customers.CompanyName AS Customer,
+    Customers.Country,
+    MAX(Orders.OrderDate) AS LastOrderDate
+FROM Customers
+JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerID
+HAVING MAX(Orders.OrderDate) < DATE('now', '-6 months')
+ORDER BY LastOrderDate ASC;
